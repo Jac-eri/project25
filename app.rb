83089ef5795fd8c36c655e73a696344cc25618b0
Jacob Eriksson <jacob.eriksson@elev.ga.ntig.se>
2025-02-04 11:16:12 +0100

    Push 1
    Start of layout

--- app.rb
+++ app.rb
@@ -3,3 +3,29 @@ require 'slim'
 require 'sqlite3'
 require 'sinatra/reloader'
 require 'becrypt'
+
+def connect_to_db(path)
+    db = SQLite3::Database.new(path)
+    db.results_as_hash = true
+    return db
+end
+
+
+post('/cards')
+
+end
+
+
+post('/login')
+
+end
+
+
+post('/profile')
+
+end
+
+
+post('/start')
+
+end
